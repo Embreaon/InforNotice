445fb5ed91647c94d77f6a122ac80a2b62ee3839
Embreaon <qhdtnals9803@gmail.com>
2023-11-07 11:46:11 +0900

    Revert "Update models.py"
    This reverts commit 3ad14a687da0c4df85682522e78e69f908de6dcb.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, Float, String
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
+from typing import List, Optional
 
 Base = declarative_base()
 
