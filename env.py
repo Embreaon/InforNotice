f29cd570a659d8604343f78f01cda1f0efaf1f12
Embreaon <qhdtnals9803@gmail.com>
2024-02-01 16:59:29 +0900

    DB Sql added

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 USER = "Admin"
 PASSWORD = "1234"
-HOST = "172.27.54.159" # 학원 IP
-# HOST = "172.21.179.21" # 노트북 IP
+HOST = "192.168.207.17" # MyPC IP
+# HOST = "172.21.179.21" # Lab IP
 PORT = "3306"
 DBNAME = "food_health_info"
